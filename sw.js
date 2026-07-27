183dbf3cf5dcd7acd61afe6f7448e379aef8d4fa
rejuanhenry-coder <rejuan.henry@yahoo.com>
2026-07-26 22:18:21 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "forged-physique-v10";
+const CACHE_NAME = "forged-physique-v11";
 const APP_SHELL = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
  
 self.addEventListener("install", (event) => {
